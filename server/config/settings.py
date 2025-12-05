4b1e93f86868ccd9778762c6b22b8e2283180819
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-12-05 03:24:00 -0800

    change allowed hosts for render

--- server/config/settings.py
+++ server/config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-dev-key-change-in-p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
